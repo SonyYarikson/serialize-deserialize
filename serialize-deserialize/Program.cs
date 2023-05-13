a12e503d7b914adc931bbd2a1116b38d37f68dad
yaros <yaros@LAPTOP-FJ2D97RD>
2023-05-13 14:43:57 +0300

    Добавьте файлы проекта.

---
+++ serialize-deserialize/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json;
+
+namespace serialize_deserialize
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer programmer1 = new Programmer("Alex", "No free time");
+            string json = JsonSerializer.Serialize(programmer1);
+            Console.WriteLine(json);
+            var restoredProgrammer = JsonSerializer.Deserialize<Programmer>(json);
+            Console.WriteLine(restoredProgrammer.FreeTime);
+
+        }
+    }
+}
---
+++ serialize-deserialize/Programmer.cs
@@ -0,0 +1,16 @@
+﻿    namespace serialize_deserialize
+{
+    internal partial class Program
+    {
+        public class Programmer
+        {
+            public string FreeTime { get; set; }
+            public string Name { get; set; }
+            public Programmer (string name, string freetime)
+            {
+                FreeTime = freetime;
+                Name = name;
+            }
+        }
+    }
+}
